792990d00993e1764015d0252b6b8a4ab8067a91
adelmarinho <adeljanmarinho@gmail.com>
2023-07-27 10:41:07 -0300

    changes database script

--- code_TCC/database.py
+++ code_TCC/database.py
@@ -2,8 +2,19 @@ import pandas as pd
 import json
 import os
 
-# base_attributes = [active_state_connection_identifier, active_state, arm_voltage, temperature_cpu, imu_acceleration_x, imu_acceleration_y, imu_acceleration_z, imu_angular_velocity_x, imu_angular_velocity_y, imu_angular_velocity_z, tool_pose_x, tool_pose_y, tool_pose_z, tool_pose_theta_x, tool_pose_theta_y, tool_pose_theta_z, tool_twist_linear_x, tool_twist_linear_y, tool_twist_linear_z, tool_twist_angular_x, tool_twist_angular_y, tool_twist_angular_z, tool_external_wrench_force_x, tool_external_wrench_force_y, tool_external_wrench_force_z, tool_external_wrench_torque_x, tool_external_wrench_torque_y, tool_external_wrench_torque_z, commanded_tool_pose_x, commanded_tool_pose_y, commanded_tool_pose_z, commanded_tool_pose_theta_x, commanded_tool_pose_theta_y, commanded_tool_pose_theta_z]
-# actuator_attributes = [command_id, status_flags, jitter_comm, position, velocity, torque, current_motor, voltage, temperature_motor, temperature_core]
+base_attributes = ['active_state_connection_identifier', 'active_state', 'arm_voltage', 'temperature_cpu',
+                   'imu_acceleration_x', 'imu_acceleration_y', 'imu_acceleration_z', 'imu_angular_velocity_x',
+                   'imu_angular_velocity_y', 'imu_angular_velocity_z', 'tool_pose_x', 'tool_pose_y', 'tool_pose_z',
+                   'tool_pose_theta_x', 'tool_pose_theta_y', 'tool_pose_theta_z', 'tool_twist_linear_x',
+                   'tool_twist_linear_y', 'tool_twist_linear_z', 'tool_twist_angular_x', 'tool_twist_angular_y',
+                   'tool_twist_angular_z', 'tool_external_wrench_force_x', 'tool_external_wrench_force_y',
+                   'tool_external_wrench_force_z', 'tool_external_wrench_torque_x', 'tool_external_wrench_torque_y',
+                   'tool_external_wrench_torque_z', 'fault_bank_a', 'fault_bank_b', 'warning_bank_a', 'warning_bank_b',
+                   'commanded_tool_pose_x', 'commanded_tool_pose_y', 'commanded_tool_pose_z',
+                   'commanded_tool_pose_theta_x', 'commanded_tool_pose_theta_y', 'commanded_tool_pose_theta_z']
+actuator_attributes = ['command_id', 'status_flags', 'jitter_comm', 'position', 'velocity', 'torque', 'current_motor',
+                       'voltage', 'temperature_motor', 'temperature_core', 'fault_bank_a', 'fault_bank_b',
+                       'warning_bank_a', 'warning_bank_b']
 # interconnect_attributes = [feedback_id, status_flags, jitter_comm, voltage, temperature_core, gripper_feedback]
 # motor_attibutes = [motor_id, position, velocity, current_motor, temperature_motor]
 # gripper_attributes = []
@@ -14,84 +25,102 @@ JSON_FILES_FOLDER = "json_data_files"
 CSV_FILES_FOLDER = "csv_data_files"
 
 
+def find_string(start_string: str, end_string: str, data: dict, instance, addition_term=0):
+    start_index = data[instance].find(start_string) + len(start_string) + addition_term
+    end_index = data[instance].find(end_string, start_index)
+    string_data = data[instance][start_index:end_index]
 
+    return string_data
 
 
 def parse_base(data: dict):
     """
-        Method to return a dictionary of the base data
-        """
+    Method to return a dictionary of the base data
+    """
 
     base_dict = {}
+    base_attributes_return = {}
 
-    for instance in instances:
-        start_index = data[instance].find("base {\n  ") + len("base {\n  ")
-        end_index = data[instance].find("\n}", start_index)
-        base_data = data[instance][start_index:end_index]
+    for instance in data.keys():
+        base_data = find_string("base {\n  ", "\n}", data, instance)
 
-        base_dict[instance] = []
+        base_dict[instance] = {}
 
         lines = base_data.split("\n  ")
+
         for line in lines:
             key, value = line.split(': ')
-            base_dict[instance].append(value.strip())
+            base_dict[instance][key.strip()] = value.strip()
 
-        return base_dict
+        base_attributes_return[instance] = []
+
+        for attribute in base_attributes:
+            list_item = base_dict[instance][attribute] if attribute in list(base_dict[instance].keys()) else " "
+            base_attributes_return[instance].append(list_item)
+
+    return base_attributes_return
 
 
 def parse_actuators(data: dict):
     """
-        Method to return a list of dictionaries of the actuators data
-        """
+    Method to return a list of dictionaries of the actuators data
+    """
 
     actuators = [f'Actuator {i + 1}' for i in range(6)]
     actuators_data = {}
     actuator_dict = {}
+    actuator_attributes_return = {}
 
-    for instance in instances:
+    for instance in data.keys():
+        actuators_data[instance] = {}
+        actuator_dict[instance] = {}
+        actuator_attributes_return[instance] = {}
         for actuator in actuators:
-            actuators_data[instance] = {}
             actuators_data[instance][actuator] = []
-            start_index = data[instance].find("actuators {") + len("actuators {")
-            end_index = data[instance].find("\n}", start_index)
-            actuators_data[instance][actuator] = data[instance][start_index:end_index].strip()
+            start_index = 0 if 'Actuator 1' else data[instance].find("actuators {") + len("actuators {")
+            actuators_data[instance][actuator] = find_string("actuators {\n ", "\n}", data, instance,
+                                                             actuators.index(actuator) * start_index)
 
-            actuator_dict[instance] = {}
+            actuator_dict[instance][actuator] = {}
             lines = actuators_data[instance][actuator].split('\n ')
-            values = []
             for line in lines:
                 key, value = line.split(':')
-                values.append(value.strip())
-            actuator_dict[instance][actuator] = values
-            values.clear()
+                actuator_dict[instance][actuator][key.strip()] = value.strip()
+
+            actuator_attributes_return[instance][actuator] = []
+            for attribute in actuator_attributes:
+                list_item = actuator_dict[instance][actuator][attribute] if attribute in list(actuator_dict[instance][actuator].keys()) else " "
+                actuator_attributes_return[instance][actuator].append(list_item)
+
+    return actuator_attributes_return
 
 
-    #TODO: insert this dictionary into a pandas dataframe
-    # return actuator_dict
-    return actuators_data
+def parse_interconnect(data: dict):
+    """
+    Method to return a list of dictionaries of the interconnect data
+   """
+    interconnect_dict = {}
 
-# def parse_interconnect(data):
-#     start_index = data.find("interconnect {")
-#     end_index = data.find("}", start_index) + 1
-#     interconnect_data = data[start_index:end_index]
-#     interconnect_dict = {}
-#     lines = interconnect_data.split("\n")
-#     for line in lines:
-#         if line.strip():
-#             key, value = line.split(":")
-#             interconnect_dict[key.strip()] = value.strip()
-#     return interconnect_dict
+    for instance in data.keys():
+        interconnect_dict[instance] = {}
+        # feedback_id = find_string(data, instance,,
+        interconnect_data = find_string("interconnect {\n", "\n}",  data, instance)
+
+        lines = interconnect_data.split("\n")
+        for line in lines:
+            key, value = line.split(': ')
+            interconnect_dict[instance][key.strip()] = value.strip()
+
+    return interconnect_dict
 
 
 for file in os.listdir(JSON_FILES_FOLDER):
     with open(f'{JSON_FILES_FOLDER}/{file}', 'r') as json_file:
         json_data = json.load(json_file)
 
-instances = list(json_data.keys())
-
 base_data_dict = parse_base(json_data)
-actuators_data_dict = parse_actuators(json_data)
-# interconnect_data_dict = parse_interconnect(json_string)
+# actuators_data_dict = parse_actuators(json_data)
+# interconnect_data_dict = parse_interconnect(json_data)
 
 
 # combined_dict = {
@@ -101,9 +130,9 @@ actuators_data_dict = parse_actuators(json_data)
 # }
 
 # Print the combined dictionary
-print(actuators_data_dict)
+# print(actuators_data_dict)
 print(base_data_dict)
-
+# print(interconnect_data_dict)
 
 
 
