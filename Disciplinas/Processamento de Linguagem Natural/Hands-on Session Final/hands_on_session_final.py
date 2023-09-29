d98c710bb1f5192ea9004b374dc6ade00894c4f1
adelmarinhe <124371746+adelmarinhe@users.noreply.github.com>
2023-09-29 14:18:02 -0300

    projeto NLP script python

--- Disciplinas/Processamento de Linguagem Natural/Hands-on Session Final/hands_on_session_final.py
+++ Disciplinas/Processamento de Linguagem Natural/Hands-on Session Final/hands_on_session_final.py
@@ -8,6 +8,6 @@ try:
     i = eng.parse(name)
     p,e = i.__next__()
     print(e)
-    print("You have written: " + eng.linearize(e))
+    print("Sentence written: " + eng.linearize(e))
 except gf.ParseError:
     print("This is not a valid sentence of FoodsEngRGL.")
